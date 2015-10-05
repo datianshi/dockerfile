3e1226d127559882f109ca63e0dad57da9197d06
Shaozhen Ding <dsz0111@gmail.com>
2015-10-05 10:50:16 -0500

    add docker files

--- spring-music/start-tomcat.sh
+++ spring-music/start-tomcat.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -ex
 
-export JAVA_OPTS="-Dport.http=$PORT"
+export JAVA_OPTS="-Dport.http=$PORT -Xmx382293K -XX:MaxMetaspaceSize=64M -Xss995K -Xms382293K -XX:MetaspaceSize=64M"
 /usr/local/tomcat/bin/catalina.sh run
